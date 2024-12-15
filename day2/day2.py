b44081f633638b9e70d0031956845b4debb5212a
dimdung <dimdung@live.com>
2024-12-15 02:59:38 -0500

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,5 @@
 ########################################
-# day1 of Advent Of Code 2024          #
+# day2 of Advent Of Code 2024          #
 # https://adventofcode.com/2024/day/1  #
 # dimdung                              #
 ########################################
