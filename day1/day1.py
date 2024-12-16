884f3edba3f8fe2ebb74486a4b7b93837b86a0fa
dimdung <dimdung@live.com>
2024-12-16 10:26:27 -0500

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -14,8 +14,8 @@ list1, list2 = list(map(list, zip(*lines)))
 
 # Part1 result 
 part1 = sum(abs(x1 - x2) for x1, x2 in zip(sorted(list1), sorted(list2)))
-print(f"The Day1 Puzzle input Part 1: {part1}")
+print(f"The Day1 Puzzle input # Part 1: {part1}")
 
 # Part2 result 
 part2 = sum(x * len([y for y in list2 if y == x]) for x in list1)
-print(f"The Day1 Puzzle input Part 2: {part2}")
+print(f"The Day1 Puzzle input # Part 2: {part2}")
