48996fd80b47a29537801384aeb7ec3a9481d926
dimdung <dimdung@live.com>
2024-12-16 10:16:10 -0500

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -7,7 +7,7 @@
 import sys
 
 
-def report_safe(nums: list[int]) -> bool:
+def report_safe_day2(nums: list[int]) -> bool:
     diffs = [abs(x1 - x2) for x1, x2 in zip(nums, nums[1:])]
     if not all(1 <= d <= 3 for d in diffs):
         return False
@@ -18,23 +18,23 @@ def report_safe(nums: list[int]) -> bool:
     return False
 
 
-def check_report(line: str, part1: bool = True) -> bool:
+def check_report_day2(line: str, part1: bool = True) -> bool:
     nums = list(map(int, line.split(" ")))
-    if report_safe(nums):
+    if report_safe_day2(nums):
         return True
     if part1:
         return False
     for i in range(len(nums)):
-        if report_safe(nums[:i] + nums[i + 1 :]):
+        if report_safe_day2(nums[:i] + nums[i + 1 :]):
             return True
     return False
 
 
 with open(sys.argv[1], "r") as f:
     lines = f.readlines()
-
-part1 = len([r for r in lines if check_report(r)])
-print(f"Part 1: {part1}")
-
-part2 = len([r for r in lines if check_report(r, part1=False)])
-print(f"Part 2: {part2}")
+# Part1 result 
+part1 = len([r for r in lines if check_report_day2(r)])
+print(f"The Day2 Puzzle input Part 1: {part1}")
+# Part2 result 
+part2 = len([r for r in lines if check_report_day2(r, part1=False)])
+print(f"The Day2 Puzzle input Part 2: {part2}")
